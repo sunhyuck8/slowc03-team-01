6d4e8be5145c7d57fe8e2f43c18824f848102cfe
Sun-Hyuck-Yoo <awfullysh82@gmail.com>
2024-05-26 17:01:20 +0900

    test : first commit
    첫번째 커밋

---
+++ src/main/java/slocw/team/slowc03/Slowc03Application.java
@@ -0,0 +1,13 @@
+package slocw.team.slowc03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slowc03Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slowc03Application.class, args);
+	}
+
+}
---
+++ src/test/java/slocw/team/slowc03/Slowc03ApplicationTests.java
@@ -0,0 +1,13 @@
+package slocw.team.slowc03;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Slowc03ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
